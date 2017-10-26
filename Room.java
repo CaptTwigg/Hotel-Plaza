8afa0ea2df3a4ff82cf6f113525bd866d51823ac
Mike Hemmje Jahn <bob@Mike-Hemmje-Jahn-Air.local>
2017-10-26 10:30:55 +0200

    hehj

--- Room.java
+++ Room.java
@@ -5,6 +5,10 @@ boolean internetAcces;
 int price;
 int floor;
 
+public void changePrice(int price){
+  this.price = price;
+}
+
 public String toString(){
   return String.format("roomID: %d numberOfBeds: %d internetAcces: %b price: %d floor: %d",
                        roomID, numberOfBeds, internetAcces, price, floor);
