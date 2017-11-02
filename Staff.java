321c113b08073650cd292aceb3f61bfaf8cf5053
DMZ <MD@tests-MacBook-Pro.local>
2017-11-02 09:49:10 +0100

    hallo

--- Staff.java
+++ Staff.java
@@ -1,15 +1,14 @@
 
 
 public class Staff {
-public static void public static void main(String[] args) {
 
 }
 
-String firstName;
-String lastName;
-String titel;
-int telephonenumber;
-int salary;
+private String  firstName;
+private String lastName;
+private String titel;
+private int telephonenumber;
+private int salary;
 
 
 public Staff(String firstName, String lastName, String titel, int telephonenumber,int salary){
@@ -34,8 +33,86 @@ public void staffFile(String file) throws Exception
   writer.close();
 
 
+//array for stafflist or open the staffile.dat >> maybe both
+String [ ][ ] staffile =  [ 4 ] [ 5 ] ;
 
 
+
+
+
+
+
+
+public staffreader(string file) throws Exception
+Scanner stafffile =new scanner (staff.dat);
+ try {
+			FileReader staffreader = new FileReader(new File("staff.dat"));
+			Scanner scan = new Scanner(staffreader);
+			while(scan.hasNextLine()){
+				System.out.println(scan.nextLine());
+			}
+			scan.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("Error Reading File");
+			e.printStackTrace();
+		}
+
+
+public  staff() {
+  return name;
+}
+
+public String getFirstName() {
+  return firstName;
+}
+
+public void setFirstName(String firstName) {
+  this.firstName = firstName;
+}
+
+public String getLastName() {
+  return lastName;
+}
+
+public void setLastName(String lastName) {
+  this.lastName = lastName;
+}
+
+public String gettitel() {
+  return titel;
+}
+
+public void settitel(String titel) {
+  this.titels = titel;
+}
+
+public int getPhoneNumber() {
+  return phoneNumber;
+}
+
+public void setPhoneNumber(int phoneNumber) {
+  this.phoneNumber = phoneNumber;
+}
+
+public int getsalary() {
+  return salary;
+}
+
+public void setsalary(int salary) {
+  this.salary = salary;
+}
+}
+
+
+{
+public class stafffileopener {
+{
+
+}
+
+
+
+}
 }
 }
 }
