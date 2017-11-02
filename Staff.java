1ccc93970ded103927da9af66011f9b9d259ac8c
Mike Hemmje Jahn <bob@Mike-Hemmje-Jahn-Air.local>
2017-11-02 09:58:09 +0100

    Fixed

--- Staff.java
+++ Staff.java
@@ -1,133 +1,46 @@
-
+import java.util.*;
+import java.io.*;
 
 public class Staff {
-
-}
-
-private String  firstName;
+private String firstName;
 private String lastName;
 private String titel;
 private int telephonenumber;
 private int salary;
 
 
-public Staff(String firstName, String lastName, String titel, int telephonenumber,int salary){
-
-this.firstName=firstName;
-this.lastName=lastName;
-this.titel=titel;
-this.telephonenumber=telephonenumber;
-this.salary=salary;
-
-
-{
-public string toString(){
-return String.format
-
-}
-public void staffFile(String file) throws Exception
-  FileWriter writer = new FileWriter(new File(file), true)
-  String staff = String.format("firstName: %s : %s lastName: %s titel: %d int: %d salary");
-  writer.write(Staff + "\n");
-  writer.flush();
-  writer.close();
-
-
-//array for stafflist or open the staffile.dat >> maybe both
-String [ ][ ] staffile =  [ 4 ] [ 5 ] ;
-
-
-
-
-
-
-
-
-public staffreader(string file) throws Exception
-Scanner stafffile =new scanner (staff.dat);
- try {
-			FileReader staffreader = new FileReader(new File("staff.dat"));
-			Scanner scan = new Scanner(staffreader);
-			while(scan.hasNextLine()){
-				System.out.println(scan.nextLine());
-			}
-			scan.close();
-		} catch (FileNotFoundException e) {
-			System.out.println("Error Reading File");
-			e.printStackTrace();
-		}
-
-
-public  staff() {
-  return name;
-}
-
-public String getFirstName() {
-  return firstName;
-}
-
-public void setFirstName(String firstName) {
+public Staff(String firstName, String lastName, String titel, int telephonenumber, int salary){
   this.firstName = firstName;
-}
-
-public String getLastName() {
-  return lastName;
-}
-
-public void setLastName(String lastName) {
   this.lastName = lastName;
-}
-
-public String gettitel() {
-  return titel;
-}
-
-public void settitel(String titel) {
-  this.titels = titel;
-}
-
-public int getPhoneNumber() {
-  return phoneNumber;
-}
-
-public void setPhoneNumber(int phoneNumber) {
-  this.phoneNumber = phoneNumber;
-}
-
-public int getsalary() {
-  return salary;
-}
-
-public void setsalary(int salary) {
+  this.titel = titel;
+  this.telephonenumber = telephonenumber;
   this.salary = salary;
 }
+
+
+public void staffFile(String file) throws Exception {
+  FileWriter writer = new FileWriter(new File(file), true);
+  String staff = String.format("firstName: %s : %s lastName: %s titel: %d int: %d salary");
+  writer.write("\n");
+  writer.flush();
+  writer.close();
 }
 
+//array for stafflist or open the staffile.dat >> maybe both
+// String [ ][ ] staffile =  [ 4 ] [ 5 ] ;
 
-{
-public class stafffileopener {
-{
-
+public void staffreader(String file) throws Exception {
+  Scanner stafffile = new Scanner("staff.dat");
+  try {
+    FileReader staffreader = new FileReader(new File("staff.dat"));
+    Scanner scan = new Scanner(staffreader);
+    while (scan.hasNextLine()) {
+      System.out.println(scan.nextLine());
+    }
+    scan.close();
+  } catch (FileNotFoundException e) {
+    System.out.println("Error Reading File");
+    e.printStackTrace();
+  }
 }
-
-
-
-}
-}
-}
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
