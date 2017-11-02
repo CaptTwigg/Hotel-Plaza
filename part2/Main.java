2e0d4b083c2d63a1c073eb4b10578aa62130835c
CaptTwigg <mike@familien-jahn.dk>
2017-11-02 15:44:32 +0100

    part2 now has own folder

---
+++ part2/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+public static void main(String[] args){
+  Employee emp = new Employee("bob", "kageMester", 22);
+  Director dir = new Director("bob", "kageMester", 22);
+
+  System.out.println(emp);
+  System.out.println(dir);
+}
+}
